a5635fcf5f918bcb8f1f142852cfa51520d4dea1
Radoslav Tsvetkov <geomilevinc@gmail.com>
2019-11-21 16:48:06 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            {/* <Navigation /> */}
+            <div className="Container">
+                <h1>Hello World!</h1>
+                {/* <Header /> */}
+                {/* <Main /> */}
+            </div>
+            {/* <Footer /> */}
+        </div>
+    );
+}
+
+export default App;
