30c764b9ad74226ae8f7c9f5d1a2d2a2d77e5217
Rio <geomilevinc@gmail.com>
2019-12-11 15:41:15 +0200

    Modified Login

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import './style.css';
+import withForm, { useFormControl, getValidationsRunnerForSchema } from '../hocs/withForm';
 
-function Login() {
-    return (
-        <form className="box" action="#" method="post">
-            <h1>Вход</h1>
-            <input type="text" name="" placeholder="Username" />
-            <input type="password" name="" placeholder="Password" />
-            <input type="submit" name="" value="Login" />
-        </form>
-    )
+class Login extends React.Component {
+    state = { error: null };
+
+    usernameChangeHandler = this.props.controlChangeHandlerFactory('username');
+    passwordChangeHandler = this.props.controlChangeHandlerFactory('password');
+
+    submitHandler = () => {
+        const errors = this.props.getFormErrorState();
+        if (!!errors) { return; }
+        const data = this.props.getFormState();
+        this.props.login(this.props.history, data).catch(error => {
+            this.setState({ error });
+        });
+    }
+
+    render() {
+        const { error } = this.state;
+
+        return (
+            <form className="box">
+                <h1>Вход</h1>
+                <div className="form-control">
+                    <input type="text" name="" placeholder="Username" onChange={this.usernameChangeHandler} />
+                </div>
+                <div className="form-control">
+                    <input type="password" name="" placeholder="Password" onChange={this.passwordChangeHandler} />
+                </div>
+                {error && error}
+                <div className="form-control">
+                    <input type="submit" name="" value="Login" onClick={this.submitHandler} />
+                </div>
+            </form>
+        )
+    }
 }
 
-export default Login;
+export default withForm(Login, { username: '', password: '' });
